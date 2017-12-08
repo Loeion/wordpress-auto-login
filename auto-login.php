40682e72cde3a8de19cfa77636ae82ec648f952f
Gulshan <32733940+gulshanbasouli@users.noreply.github.com>
2017-12-08 15:45:49 +0530

    Update

--- auto-login.php
+++ auto-login.php
@@ -1,7 +1,7 @@
 <?php 
 
   $userDetail = get_user_by('email', 'gulshan.webchefz@gmail.com');
-  $userID = $user->ID
+  $userID = $user->ID;
   wp_set_current_user($userID, 'gulshan.webchefz@gmail.com');
   wp_set_auth_cookie($userID);
   do_action('wp_login', 'gulshan.webchefz@gmail.com');
